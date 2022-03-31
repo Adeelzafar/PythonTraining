14c3b20cec8577b4ab09e7fb48f30ba6c46cf375
Adeelzafar <adeelzafar.pk@gmail.com>
2022-03-31 11:51:12 +0500

    Add files via upload

---
+++ day3-activity-2.py
@@ -0,0 +1,19 @@
+dictionary_days = dict()                       # Initializes the dictionary
+fname = input('Enter a file name: ')
+try:
+    fhand = open(fname)
+except FileNotFoundError:
+    print('File cannot be opened:', fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != 'From':
+        continue
+    else:
+        if words[2] not in dictionary_days:
+            dictionary_days[words[2]] = 1       # First entry
+        else:
+            dictionary_days[words[2]] += 1      # Additional counts
+
+print(dictionary_days)
